f091801728c40c5afca8c6ed03b0808845e05dc5
Kévin BUANNEC <noxtariadev@gmail.com>
2023-07-06 14:50:24 +0200

    Spring API Validator / Spring Boot DTO

---
+++ TP/TP_validator_SPRING/src/main/java/com/example/tp_validator_spring/TpValidatorSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp_validator_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpValidatorSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpValidatorSpringApplication.class, args);
+    }
+
+}
