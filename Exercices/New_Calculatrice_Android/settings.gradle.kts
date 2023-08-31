7f0d52bedd7c8108180685d6d02d0a2fe1068b59
Kévin BUANNEC <kevin.buannec@proton.me>
2023-08-31 11:27:19 +0200

    Ajout Affichage calculatrice sur Android

---
+++ Exercices/New_Calculatrice_Android/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Calculatrice2.0_Android"
+include(":app")
+ 
